73dfa76b79519c1be02eb2eb9dad999509fdfc94
Spencer Dodd <spencerdodd@Spencers-MacBook-Pro.local>
2016-09-07 12:08:19 -0400

    text to speech module added

--- src/alarm.py
+++ src/alarm.py
@@ -8,10 +8,14 @@ On alarm:
 """
 
 import os
-import json	# for credentials
+import json
+import time
 import pydub # for splicing alarm and alert audio
+import pyttsx
 import datetime
 import httplib2
+import threading
+import subprocess
 from apiclient import discovery
 import oauth2client
 from oauth2client import client
@@ -24,13 +28,29 @@ try:
 except ImportError:
     flags = None
 
+### ===========================================================================
+### ==== CREDENTIAL VARIABLES =================================================
+### ===========================================================================
 # If modifying these scopes, delete your previously saved credentials
 # at ~/.credentials/calendar-python-quickstart.json
 SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
 CLIENT_SECRET_FILE = 'client_secret.json'
-APPLICATION_NAME = 'Google Calendar API Python Quickstart'
+APPLICATION_NAME = 'Google Calendar API Pynamic Alarm'
 BASE_DIR = os.path.normpath(os.getcwd() + os.sep + os.pardir + os.sep)
 
+### ===========================================================================
+### ==== APPLICATION VARIABLES ================================================
+### ===========================================================================
+alarm_cutoff_time = 4 # Earliest time an alarmed event can start (24hr clock)
+
+### ===========================================================================
+### ==== DATA STRUCTURES ======================================================
+### ===========================================================================
+
+
+### ===========================================================================
+### ==== METHODS ==============================================================
+### ===========================================================================
 def get_credentials():
 	"""Gets valid user credentials from storage.
 
@@ -45,12 +65,12 @@ def get_credentials():
 	if not os.path.exists(credential_dir):
 		os.makedirs(credential_dir)
 	credential_path = os.path.join(credential_dir,
-								   'calendar-python-quickstart.json')
+								   'calendar-python-alarm.json')
 
 	store = oauth2client.file.Storage(credential_path)
 	credentials = store.get()
 	if not credentials or credentials.invalid:
-		flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+		flow = client.flow_from_clientsecrets(os.path.join(home_dir, CLIENT_SECRET_FILE), SCOPES)
 		flow.user_agent = APPLICATION_NAME
 		if flags:
 			credentials = tools.run_flow(flow, store, flags)
@@ -59,11 +79,9 @@ def get_credentials():
 		print('Storing credentials to ' + credential_path)
 	return credentials
 
-def main():
-	"""Shows basic usage of the Google Calendar API.
-
-	Creates a Google Calendar API service object and outputs a list of the next
-	10 events on the user's calendar.
+def get_upcoming_events():
+	"""Creates a Google Calendar API service object and outputs a list of the 
+	next 10 events on the user's calendar.
 	"""
 	credentials = get_credentials()
 	http = credentials.authorize(httplib2.Http())
@@ -81,6 +99,34 @@ def main():
 	for event in events:
 		start = event['start'].get('dateTime', event['start'].get('date'))
 		print(start, event['summary'])
+
+def text_to_speech(text):
+	"""Speaks the given text as audio on the device running the alarm app.
+	"""
+	message = "{}".format(text)
+	
+	engine = pyttsx.init()
+	engine.say(message)
+	engine.runAndWait()
+	
+	# sleep for as long as it would take to say the text
+	print "speaking"
+	time.sleep(len(text) / 5)
+	print "done speaking"
+	return 0
+
+def main():
+	threads = []
+	comments = [
+		"This is just a test test test test test"
+	]
+	for i in comments:
+		t = threading.Thread(target=text_to_speech, args=(i,))
+		t.start()
+
+	while True:
+		time.sleep(1)
+		print "running"
 
 
 if __name__ == '__main__':
