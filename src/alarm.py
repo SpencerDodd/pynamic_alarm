433cb2bc9c950a7c4679575aa829803dfa419239
Spencer Dodd <spencerdodd@Spencers-MacBook-Pro.local>
2016-09-07 16:39:15 -0400

    functional model

--- src/alarm.py
+++ src/alarm.py
@@ -6,15 +6,11 @@ On alarm:
 	play alarm tone
 	text to speech name of calendar event
 """
-# error checks
-import faulthandler
-faulthandler.enable()
 
 import os
 import sys
 import json
 import time
-import pydub # for splicing alarm and alert audio
 import pyttsx
 import datetime
 import httplib2
@@ -46,21 +42,32 @@ BASE_DIR = os.path.normpath(os.getcwd() + os.sep + os.pardir + os.sep)
 ### ===========================================================================
 alarm_cutoff_time = 4 # Earliest time an alarmed event can start (24hr clock)
 alarms = {} # dict: key = time of alarm, value = AlarmAlert for event
+timezone_adjustments = { # adjustments to utc time
+	"est" : -4,
+}
+current_timezone = "est"
 
 ### ===========================================================================
 ### ==== DATA STRUCTURES ======================================================
 ### ===========================================================================
 class AlarmAlert:
 
-	def __init__(self, alert_time, alert_title):
-		self.alert_time = alert_time
+	def __init__(self, alert_title, alert_time, alert_location):
 		self.alert_title = alert_title
+		self.alert_time = alert_time
+		self.alert_location = alert_location
+
+		# non-input
+		self.triggered = False
+		self.acknowledged = False
 
 	def alarm_alert(self):
 		"""Alerts for an alarm.
 
 		Includes playing alarm tones and text to speech of the alarm title for
 		the alerting event.
+
+		Plays until it is acknowledged (stopped)
 		"""
 		print "Alerting for {} at {}".format(self.alert_title, self.alert_time)
 		message = "{}".format(self.alert_title)
@@ -82,9 +89,21 @@ class AlarmAlert:
 		return
 
 	def run(self):
-		t = threading.Thread(target=self.alarm_alert)
-		t.start()
+		if not self.triggered:
+			t = threading.Thread(target=self.alarm_alert)
+			t.start()
+			self.triggered = True
 
+	### ==== TEST METHODS =============================
+	def test_alert(self):
+		print "="*30
+		print "Alerting for {} at {}".format(self.alert_title, self.alert_time)
+		print "="*30
+
+	def test_run(self):
+		t = threading.Thread(target=self.test_alert)
+		t.start()
+	### ===============================================
 
 ### ===========================================================================
 ### ==== METHODS ==============================================================
@@ -128,23 +147,97 @@ def get_upcoming_events():
 	service = discovery.build('calendar', 'v3', http=http)
 
 	now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-	print('Getting the upcoming 10 events')
+	print('Searching for upcoming events ...')
 	eventsResult = service.events().list(
 		calendarId='primary', timeMin=now, maxResults=10, singleEvents=True,
 		orderBy='startTime').execute()
 	events = eventsResult.get('items', [])
+	
+	return events
+	
 
-	if not events:
-		print('No upcoming events found.')
+def update_alarms():
+	""" This method gets the next 10 events, or the next two days worth of
+	events (whichever set is larger). It then compares these events to existing
+	alarms (based on timestamp and name). If there are new events that are not
+	in the current alarm set, a new AlarmAlert object is created and added to
+	the alarm set. 
+
+	Previously alerted events are also removed from the alarm set. TODO
+	"""
+	events = get_upcoming_events()
+
 	for event in events:
-		start = event['start'].get('dateTime', event['start'].get('date'))
-		print(start, event['summary'])
+		event_name = event.get('summary')
+		event_time = event.get('start').get('dateTime')
+		event_location = event.get('location')
+
+		# if there isn't an alarm at this event's time, add it
+		if event_time not in alarms.keys() and not_before_cutoff(event_time):
+			create_alarm(event_name, event_time, event_location)
+			
+def not_before_cutoff(event_time):
+	""" This method checks to make sure that an event's time is not before
+	the cutoff time specified in our variables section.
+
+	Returns true if the event time is acceptable (after cutoff)
+	Returns false if the event time is unacceptable (before cutoff)
+	"""
+	return True
+
+def create_alarm(event_name, event_time, event_location):
+	""" This method creates a new alarm event with the given values.
+	"""
+	print "New event alarm:"
+	print "	Name: {}".format(event_name)
+	print "	Time: {}".format(event_time)
+	print "	Location: {}".format(event_location)
+	new_alert = AlarmAlert(event_name, event_time, event_location)
+	alarms[event_time] = new_alert
+
+def check_alarms():
+	""" This method checks the current timestamp against all of the current
+	alarms (+/- 30 seconds). If there is a match (timestamp overlap), the
+	alarm with overlap to current time is triggered (.run()).
+	"""
+	current_time = datetime.datetime.utcnow().isoformat() + 'Z'
+	for alarm_time in alarms:
+		f_current_time = format_time(current_time, 'utcnow')
+		f_alarm_time = format_time(alarm_time, 'google')
+
+		if f_current_time == f_alarm_time:
+			alarms[alarm_time].run()
+
+
+def format_time(timestring, time_format):
+	""" Formats a given timestring into YYYY-MM-DD-MM-HH according to
+	the time_format field that is given.
+	"""
+	if time_format == 'utcnow':
+		year = timestring[:4]
+		month = timestring[5:7]
+		day = timestring[8:10]
+		hour = str(
+			int(timestring[11:13]) + timezone_adjustments[current_timezone]
+			)
+		minute = timestring[14:16]
+
+		return "{}-{}-{}-{}-{}".format(year, month, day, hour, minute)
+
+	elif time_format == 'google':
+		year = timestring[:4]
+		month = timestring[5:7]
+		day = timestring[8:10]
+		hour = timestring[11:13]
+		minute = timestring[14:16]
+
+		return "{}-{}-{}-{}-{}".format(year, month, day, hour, minute)
+
 
 def main():
-	alarm = AlarmAlert("now", "Test Event")
-	alarm.run()
 	while True:
-		pass
+		update_alarms()
+		check_alarms()
 		time.sleep(1)
 
 
